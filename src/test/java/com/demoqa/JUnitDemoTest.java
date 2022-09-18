0b04cce8a3a334f16f27148404c23a757d4e37df
aziyatdin0v <azat.ziyatdinov@yandex.ru>
2022-09-18 14:57:11 +0300

    added life cycle of test in JUnit

--- src/test/java/com/demoqa/JUnitDemoTest.java
+++ src/test/java/com/demoqa/JUnitDemoTest.java
@@ -1,4 +1,40 @@
 package com.demoqa;
 
+import org.junit.jupiter.api.*;
+
 public class JUnitDemoTest {
+
+    @BeforeAll
+    static void setUp() {
+        System.out.println("### @BeforeAll");
+
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("### @AfterAll");
+
+    }
+
+    @BeforeEach
+    void beforeWEachTest() {
+        System.out.println("###     @BeforeEach");
+    }
+
+    @AfterEach
+    void afterWEachTest() {
+        System.out.println("###     @AfterEach");
+    }
+
+    @Test
+    void simpleTestFirst() {
+        System.out.println("###         @simpleTestFirst");
+        Assertions.assertTrue(3 > 2);
+    }
+
+    @Test
+    void simpleTestSecond() {
+        System.out.println("###         @simpleTestSecond");
+        Assertions.assertTrue(3 > 2);
+    }
 }
